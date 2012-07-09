5f3831304e27f08e8601b5b2e0f009ab502292c5
Paul and Kyle <kburton@relaynetwork.com>
2012-07-09 16:59:00 -0400

    register/deregister and health checks for elbs

--- aws/src/aws/core.clj
+++ aws/src/aws/core.clj
@@ -14,35 +14,35 @@
   (let [zone-records (filter #(= "CNAME" (:type %1)) (route53/zone-records (get-in request [:route-params :name])))]
     (doseq [zone-rec zone-records]
       (let [resource-name (.replaceAll (:name zone-rec) "\\.$" "")]
-       (doseq [resource (:resourceRecords zone-rec)]
-         (println (format "%s\t%s" resource-name (.getValue resource))))))))
+        (doseq [resource (:resourceRecords zone-rec)]
+          (println (format "%s\t%s" resource-name (.getValue resource))))))))
 
 (defn route53-ls [request]
   (doseq [zone (route53/hosted-zones)]
     (let [info (bean zone)]
-     (println (format "%s\t%s" zone info)))))
+      (println (format "%s\t%s" zone info)))))
 
 (defn route-matches? [route args]
   (loop [[route-part & route-parts] (:pattern route)
          [arg & args] args
          route-params {}]
     (cond
-     (and (not route-part) (not arg))
-     (assoc route :route-params route-params)
+      (and (not route-part) (not arg))
+      (assoc route :route-params route-params)
 
-     (or (not route-part)
-         (not arg))
-     nil
+      (or (not route-part)
+          (not arg))
+      nil
 
-     (and (= String (class route-part))
-          (= route-part arg))
-     (recur route-parts args route-params)
+      (and (= String (class route-part))
+           (= route-part arg))
+      (recur route-parts args route-params)
 
-     (keyword? route-part)
-     (recur route-parts args (assoc route-params route-part arg))
+      (keyword? route-part)
+      (recur route-parts args (assoc route-params route-part arg))
 
-     :no-match
-     nil)))
+      :no-match
+      nil)))
 
 (defn elb-ls [request]
   (doseq [elb (elb/load-balancers)]
@@ -53,12 +53,30 @@
   (let [elb-info (elb/load-balancer-for-resource-name (get-in request [:route-params :name]))]
     (pp/pprint elb-info)))
 
+(defn elb-remove-instance [request]
+  (let [elb-name    (get-in request [:route-params :name])
+        instance-id (get-in request [:route-params :instance])]
+    (elb/deregister-instance elb-name instance-id)))
+
+(defn elb-add-instance [request]
+  (let [elb-name    (get-in request [:route-params :name])
+        instance-id (get-in request [:route-params :instance])]
+    (elb/register-instance elb-name instance-id)))
+
+(defn elb-instance-health [request]
+  (let [elb-name    (get-in request [:route-params :name])]
+    (doseq [instance-info (elb/instance-health elb-name)]
+      (println (join "\t" (map instance-info [:instanceId :state]))))))
+
 (def routing-table
      [
       {:pattern ["route53" "ls"]        :handler route53-ls}
       {:pattern ["route53" "ls" :name]  :handler route53-ls-zone}
       {:pattern ["elb" "ls"]            :handler elb-ls}
       {:pattern ["elb" "ls" :name]      :handler elb-ls-elb}
+      {:pattern ["elb" :name "remove" :instance] :handler elb-remove-instance}
+      {:pattern ["elb" :name "add"    :instance] :handler elb-add-instance}
+      {:pattern ["elb" :name "health"] :handler elb-instance-health}
       ])
 
 
@@ -77,6 +95,34 @@
       matching-route
       (do
         (println (format "match: %s" matching-route))
-       ((:handler matching-route) matching-route))
+        ((:handler matching-route) matching-route))
       :no-matching-route
       (println (format "no matching command for: %s" args)))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- aws/src/aws/elb.clj
+++ aws/src/aws/elb.clj
@@ -2,16 +2,21 @@
   (:require
    [aws.route53 :as route53])
   (:import
-   [com.amazonaws.services.elasticloadbalancing AmazonElasticLoadBalancingClient])
+   [com.amazonaws.services.elasticloadbalancing AmazonElasticLoadBalancingClient]
+   [com.amazonaws.services.elasticloadbalancing.model
+    RegisterInstancesWithLoadBalancerRequest
+    DeregisterInstancesFromLoadBalancerRequest
+    Instance
+    DescribeInstanceHealthRequest])
   (:use
    aws.aws
    [clj-etl-utils.cache-utils :only [def-simple-cached]]
    [clj-etl-utils.lang-utils :only [raise]]))
 
-
+(def elb-client (AmazonElasticLoadBalancingClient. aws-credentials))
 
 (def-simple-cached load-balancers []
-  (vec (.getLoadBalancerDescriptions (.describeLoadBalancers (AmazonElasticLoadBalancingClient. aws-credentials)))))
+  (vec (.getLoadBalancerDescriptions (.describeLoadBalancers elb-client))))
 
 (defn load-balancer-for-resource-name [resource-name]
   (let [elb-dns-name  (.getValue (first (:resourceRecords (first (route53/records-for-resource resource-name)))))]
@@ -20,4 +25,36 @@
       (fn [elb]
         (= elb-dns-name (:DNSName elb)))
       (map bean (load-balancers))))))
+
+(defn deregister-instance [elb-dns-name instance-id]
+  (let [elb-info (load-balancer-for-resource-name elb-dns-name)]
+    elb-info
+    (.deregisterInstancesFromLoadBalancer
+     elb-client
+     (DeregisterInstancesFromLoadBalancerRequest.
+      (:loadBalancerName elb-info)
+      [(Instance. instance-id)])))
+  (clj-etl-utils.cache-utils/purge-standard-caches)
+  true)
+
+(defn register-instance [elb-dns-name instance-id]
+  (let [elb-info (load-balancer-for-resource-name elb-dns-name)]
+    elb-info
+    (.registerInstancesWithLoadBalancer
+     elb-client
+     (RegisterInstancesWithLoadBalancerRequest.
+      (:loadBalancerName elb-info)
+      [(Instance. instance-id)])))
+  (clj-etl-utils.cache-utils/purge-standard-caches)
+  true)
+
+(defn instance-health [elb-dns-name]
+  (vec
+   (map
+    bean
+    (.getInstanceStates
+     (.describeInstanceHealth
+      elb-client
+      (DescribeInstanceHealthRequest.
+       (:loadBalancerName (load-balancer-for-resource-name elb-dns-name))))))))
 
