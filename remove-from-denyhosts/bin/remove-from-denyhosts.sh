66e97b68e2ec21bc03042c4afda50d5cd80d32cf
Kyle Burton <kburton@relaynetwork.com>
2012-07-16 13:22:10 +0000

    need to use sudo on all commands

--- remove-from-denyhosts/bin/remove-from-denyhosts.sh
+++ remove-from-denyhosts/bin/remove-from-denyhosts.sh
@@ -1,10 +1,11 @@
-/etc/init.d/denyhosts stop
+sudo /etc/init.d/denyhosts stop
 
-perl -i.bak -ne "print unless \$_ =~ /$1/" /etc/hosts.deny
+sudo perl -i.bak -ne "print unless \$_ =~ /$1/" /etc/hosts.deny
 
 for f in $(grep -l "$1" /var/lib/denyhosts/*); do
   echo "removing $1 from $f"
-  perl -i.bak -ne "print unless \$_ =~ /^$1/" $f
+  sudo perl -i.bak -ne "print unless \$_ =~ /^$1/" $f
 done
-/etc/init.d/denyhosts start
+
+sudo /etc/init.d/denyhosts start
 
