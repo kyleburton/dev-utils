658f7f7320b3955142631bab9c4f2d5b43e16148
Paul and Nick <kburton@relaynetwork.com>
2014-01-02 11:59:34 -0500

    glacier api support

--- aws/project.clj
+++ aws/project.clj
@@ -3,9 +3,10 @@
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :local-repo-classpath true
   :dependencies [
-    [org.clojure/clojure                  "1.3.0"]
+    [org.clojure/clojure                  "1.5.1"]
+    [org.clojure/data.json                "0.2.2"]
     [org.clojure/tools.cli                "0.2.1"]
-    [com.amazonaws/aws-java-sdk           "1.3.30"]
-    [org.clojars.kyleburton/clj-etl-utils "1.3.4"]
-    [http-kit                             "2.1.2"]
+    [com.amazonaws/aws-java-sdk           "1.6.10"]
+    [com.github.kyleburton/clj-etl-utils  "1.0.79"]
+    [http-kit                             "2.1.15"]
   ])
