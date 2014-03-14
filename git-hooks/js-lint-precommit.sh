c1601045b894057c51b5faa54585d54c1bc5eb60
Nick <kburton@relaynetwork.com>
2014-03-14 18:41:09 -0400

    jslint pre commit script

---
+++ git-hooks/js-lint-precommit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "NOTE: if you need to bypass this pre-commit hook, pass the --no-verify flag to Git"
+for i in $(git diff --cached --name-only | grep .js); do 
+  if auto-jslint -b $i; then
+    echo "ok"
+  else
+    auto-jslint -b $i
+    exit 1
+  fi
+done
