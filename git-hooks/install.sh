f1534b5490ee3168ecebc1f1839b1e7373a2a0b2
Pat <kburton@relaynetwork.com>
2015-05-15 11:08:45 -0400

    Prompt without newline

--- git-hooks/install.sh
+++ git-hooks/install.sh
@@ -6,3 +6,21 @@ echo '#!/bin/sh' >> ~/projects/rn-wall-front-end/.git/hooks/pre-commit
 cat js-lint-precommit.sh >> ~/projects/rn-wall-front-end/.git/hooks/pre-commit
 cat set-pair-precommit.sh >> ~/projects/rn-wall-front-end/.git/hooks/pre-commit
 chmod +x ~/projects/rn-wall-front-end/.git/hooks/pre-commit
+
+# rn-boomerang
+rm -f ~/projects/rn-boomerang/.git/hooks/pre-commit
+echo '#!/bin/sh' >> ~/projects/rn-boomerang/.git/hooks/pre-commit
+cat set-pair-precommit.sh >> ~/projects/rn-boomerang/.git/hooks/pre-commit
+chmod +x ~/projects/rn-boomerang/.git/hooks/pre-commit
+
+# rn-wall
+rm -f ~/projects/rn-wall/.git/hooks/pre-commit
+echo '#!/bin/sh' >> ~/projects/rn-wall/.git/hooks/pre-commit
+cat set-pair-precommit.sh >> ~/projects/rn-wall/.git/hooks/pre-commit
+chmod +x ~/projects/rn-wall/.git/hooks/pre-commit
+
+# rn-client-portal
+rm -f ~/projects/rn-client-portal/.git/hooks/pre-commit
+echo '#!/bin/sh' >> ~/projects/rn-client-portal/.git/hooks/pre-commit
+cat set-pair-precommit.sh >> ~/projects/rn-client-portal/.git/hooks/pre-commit
+chmod +x ~/projects/rn-client-portal/.git/hooks/pre-commit
--- git-hooks/set-pair-precommit.sh
+++ git-hooks/set-pair-precommit.sh
@@ -1,7 +1,6 @@
 exec < /dev/tty
 pn=`rn_pair_name`
-echo "Type enter to use pair name '$pn', or enter correct pair name:"
-read user_input
+read -p "Type enter to use pair name '$pn', or enter correct pair name: " user_input
 if [ "$user_input" = "" ]; then
   exit 0
 fi
