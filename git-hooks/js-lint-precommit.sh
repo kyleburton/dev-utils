f4808113c644ed8774348f86faba19728272d7c7
Paul and Chris <kburton@relaynetwork.com>
2015-06-04 15:58:37 -0400

    do not jslint client/libs

--- git-hooks/js-lint-precommit.sh
+++ git-hooks/js-lint-precommit.sh
@@ -1,5 +1,5 @@
 echo "NOTE: if you need to bypass this pre-commit hook, pass the --no-verify flag to Git"
-for i in $(git diff --cached --name-only | grep -v \\.min\\.js | grep \\.js | grep -v version.js); do 
+for i in $(git diff --cached --name-only | grep -v \\.min\\.js | grep \\.js | grep -v version.js | grep -v client\/libs\/ ); do 
   if auto-jslint -b $i; then
     echo "ok"
   else
